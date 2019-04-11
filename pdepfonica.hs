5c55e34619a1eb06d3ed0618bfaf36c0e3a1cbe3
PalumboN <nahuel.palumbo@gmail.com>
2019-04-11 10:19:56 -0300

    Punto 2

--- pdepfonica.hs
+++ pdepfonica.hs
@@ -1,9 +1,24 @@
-type Cliente = String
+type NumeroTelefonico = String
+type Saldo = Float
+type Cliente = (NumeroTelefonico, Saldo)
 
 alf :: Cliente
-alf = "66660000"
+alf = ("66660000", 5)
 debi :: Cliente
-debi = "77770000"
+debi = ("77770000", -20)
+
+numeroTelefonico :: Cliente -> NumeroTelefonico
+-- numeroTelefonico (numero, _) = numero
+numeroTelefonico = fst
+
+saldo :: Cliente -> Saldo
+-- saldo (_, saldo) = saldo
+saldo = snd
 
 esNuevo :: Cliente -> Bool
-esNuevo = (== '7') . head 
+esNuevo = (== '7') . head . numeroTelefonico
+
+esMoroso :: Cliente -> Bool
+esMoroso = esNegativo . saldo
+
+esNegativo = (<0)
